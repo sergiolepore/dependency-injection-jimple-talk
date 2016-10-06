72aec756659b91d33a10348339d63639a3968ba5
Sergio Lepore <sergio.d.lepore@gmail.com>
2016-10-06 02:06:01 -0300

    Chapter 1.1: Kinda IoC. "Don't call us. We'll call you"

---
+++ us-laptop.js
@@ -0,0 +1,22 @@
+const TypeBCord = require('./type-b-cord')
+
+class UsLaptop
+{
+    constructor () {
+        this.brand = 'Banana'
+        this.model = 'GrandNaine Pro'
+        this.os = 'grandOS 42 "Uritorco"'
+        this.cord = new TypeBCord()
+    }
+
+    powerOn () {
+        if (!this.cord.hasCharge()) {
+            throw new Error('No electricity! Am I plugged in? :(')
+        }
+
+        console.log(`Beep-Boop, ${this.brand} ${this.model} running!`)
+        console.log(`${this.os} booting up...`)
+    }
+}
+
+module.exports = UsLaptop
